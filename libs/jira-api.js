0f7987af02894ea1509a4eb85d0fdd935a29fae2
Ryan Regalado <ryan@design48.net>
2013-10-15 11:39:05 -0700

    being api

--- libs/jira-api.js
+++ libs/jira-api.js
@@ -1,17 +1,20 @@
 var request = require('request'); 
 
-var jiraApi = function() {
-    return {
-        init: function() {
-            console.log('starting up api');
+var jiraApi =  {
+    init: function() {
+        console.log('starting up api');
 
-            request('http://www.google.com', function(err, res, body) {
-                if (!err && res.statusCode === 200) {
-                    console.log(body);
-                }
-            });
-        }
+        this.connect();
 
+        // request('http://www.google.com', function(err, res, body) {
+        //     if (!err && res.statusCode === 200) {
+        //         console.log(body);
+        //     }
+        // });
+    },
+
+    connect: function() {
+        console.log('connecting to api');
     }
 };
 
