c2717c84ef4cdc2ea0f8f72c0aeafe188f234a81
Ryan Regalado <ryan@design48.net>
2013-10-15 14:57:14 -0700

    cleanup

--- test/jira-api.js
+++ test/jira-api.js
@@ -4,6 +4,5 @@ var jiraApi = require('./../libs/jira-api.js');
 describe('test suite for jira api', function() {
     it('should have init', function() {
         jiraApi.init().should.equal(true);
-        // true.should.equal(false);
     });
 });
